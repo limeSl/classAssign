2aa3f205380b3caca44893ef45e8666997937d57
limesL <43523423+limeSl@users.noreply.github.com>
2026-01-06 14:31:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -493,7 +493,6 @@ if uploaded is not None:
     df["이전 반"] = "1-" + df["이전 반"]
     df["표시명"] = (
         df["이름"].astype(str).str.strip()
-        + " (이전반 "
         + df["이전 반"]
         + ")"
     )
